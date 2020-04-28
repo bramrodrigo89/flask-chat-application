ca4aba554ad25787167719be09c549ba74bbb205
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-04-28 21:21:05 +0000

    added default values to IP and PORT

--- run.py
+++ run.py
@@ -32,6 +32,6 @@ def user(username):
     return render_template("chat.html", username=username, chat_messages=messages)
 
 if __name__=="__main__":
-    app.run(host=os.getenv("IP","0.0.0.0."),
+    app.run(host=os.getenv("IP","0.0.0.0"),
         port=int(os.getenv("PORT","5000")),
         debug=False)
