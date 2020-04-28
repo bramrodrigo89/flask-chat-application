2e2d04a35f2b75e4e173f7d3ce3b9ff9c3b6ff38
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-04-28 15:54:21 +0000

    Add the ability to write to and read from a list and display the messages to the user

--- run.py
+++ run.py
@@ -1,19 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages=[]
+
+def add_message(username, message):
+    """Add messages to the messages' list"""
+    messages.append("{}: {}".format(username,message))
+
+def get_all_messages():
+    """Gets all messages and separates them with a <br> tag"""
+    return "<br>".join(messages)
 
 @app.route("/")
 def index():
+    """Main page with instructions"""
     return "To send a message please use /USERNAME/MESSAGE"
 
 @app.route("/<username>")
 def user(username):
-    return "Hello, {0}".format(username)
+    """Welcome user and display stored messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username,get_all_messages())
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
-    return "{0}: {1}".format(username,message)
+    """Create a new message and redirect back to main page"""
+    add_message(username,message)
+    return redirect("/" + username)
 
 if __name__=="__main__":
     app.run(host=os.getenv("IP"),
