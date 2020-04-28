ea07eb25b2d44cee953addf0479d32d92cbd88f9
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-04-28 18:10:47 +0000

    adding timestamps, an index.html page, a user form, and session variables to store username

--- run.py
+++ run.py
@@ -1,21 +1,29 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key="randomString123"
 messages=[]
 
 def add_message(username, message):
     """Add messages to the messages' list"""
-    messages.append("{}: {}".format(username,message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now,username,message))
 
 def get_all_messages():
     """Gets all messages and separates them with a <br> tag"""
     return "<br>".join(messages)
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def index():
     """Main page with instructions"""
-    return "To send a message please use /USERNAME/MESSAGE"
+    if request.method=="POST":
+        session["username"]=request.form["username"]
+    if "username" in session:
+        return redirect(session["username"])
+        
+    return render_template("index.html")
 
 @app.route("/<username>")
 def user(username):
